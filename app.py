867b5066511c1845026852192fb25a44ca253856
M3LN1K <whynot-2@yandex.ru>
2022-05-31 03:49:16 +0300

    first

--- app.py
+++ app.py
@@ -4,64 +4,13 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 	False
-db = SQLAlchemy(app)
 
 
-'''
-class Article(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(100), nullable=False)
-	intro = db.Column(db.String(300), nullable=False)
-	text = db.Column(db.Text, nullable=False)
-	date = db.Column(db.DateTime, default=datetime.utcnow)
-
-	def __repr__(self):
-		return '<Article %r>' % self.id
-'''
 
 
-'''
+
+
 @app.route('/')
-@app.route('/home')
-def test():
-	return render_template("test.html")
-
-
-@app.route('/about')
-def about():
-	return render_template("test_two.html")
-
-@app.route('/posts')
-def posts():
-	articles = Article.query.order_by(Article.date).all()
-	return render_template("posts.html", articles=articles)
-
-
-@app.route('/add_article', methods=['POST', 'GET'])
-def add_article():
-	if request.method == "POST":
-		title = request.form['title']
-		intro = request.form['intro']
-		text = request.form['text']
-
-		article = Article(title=title, intro=intro, text=text)
-
-		try:
-			db.session.add(article)
-			db.session.commit()
-			return redirect('/')
-		except:
-			return " При добавлении статьи произошла ошибка"
-	else:
-		return render_template("add_article.html")
-'''
-
-
-
-
-
 @app.route('/home')
 def index():
 	return render_template("index.html")
@@ -215,8 +164,6 @@ def triplo():
 @app.route('/Viennese_coffee')
 def viennese_coffee():
 	return render_template("Viennese_coffee.html")
-
-
 
 
 if __name__ == "__main__":
